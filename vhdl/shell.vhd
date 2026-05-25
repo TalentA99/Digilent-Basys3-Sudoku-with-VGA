e9a02925bbf350801b429a765e55b81d32da9770
Joao F. De Araujo Junior Jr. <f006vjv@babylon1.thayer.dartmouth.edu>
2026-05-24 23:19:06 -0400

    Finished first implementation of vga, not tested

--- vhdl/shell.vhd
+++ vhdl/shell.vhd
@@ -14,7 +14,7 @@ use IEEE.NUMERIC_STD.ALL;
 --=============================================================================
 --Entity Declaration:
 --=============================================================================
-entity top_level is
+entity shell is
 	Generic(
         CLK_DIVIDER_RATIO : integer := 2);
     Port ( 	
@@ -24,7 +24,7 @@ entity top_level is
 			vgaGreen_ext_port	: out std_logic_vector(3 downto 0);
 			Hsync_port			: out std_logic;
 			Vsync_port			: out std_logic);  
-end top_level;
+end shell;
 
 --=============================================================================
 --Architecture Type:
@@ -37,7 +37,7 @@ architecture behavioral_architecture of shell is
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 --System Clock Generation:
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-component system_clock_generation is
+component clock_generation is
     	Generic( CLK_DIVIDER_RATIO : integer );
         Port (
             --External Clock:
@@ -46,20 +46,48 @@ component system_clock_generation is
             system_clk_port		: out std_logic);
 end component;
 
+component vga_test_pattern_12bit is
+	Port (
+		row, column 		: in std_logic_vector(9 downto 0);
+		color 				: out std_logic_vector(11 downto 0)
+	);
+end component;
+
+component vga_sync is
+	Port (
+		clk	        		: in std_logic; -- Assumes 25MHz clock
+		pixel_x				: out std_logic_vector(9 downto 0);
+		pixel_y				: out std_logic_vector(9 downto 0);
+		video_on			: out std_logic;
+		hsync			    : out std_logic;
+		vsync				: out std_logic
+	);
+end component;
+
 --=============================================================================
 --Signal Declarations: 
 --=============================================================================
 --Timing:
 signal clk: std_logic := '0';
+signal pixel_x, pixel_y : std_logic_vector(9 downto 0) := (others => '0');
+signal color : std_logic_vector(11 downto 0);
+signal video_on : std_logic := '0';
+
 
 --=============================================================================
 --Port Mapping (wiring the component blocks together): 
 --=============================================================================
 begin
+
+-- Wire color 
+vgaRed_ext_port <= color(11 downto 8);
+vgaGreen_ext_port <= color(7 downto 4);
+vgaBlue_ext_port <= color(3 downto 0);
+
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 --Wire the system clock generator into the shell with a port map:
 --+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-clocking: system_clock_generation
+clocking: clock_generation
         generic map (
             -- leave this so it can be passed down from the testbench
             clk_divider_ratio => clk_divider_ratio
@@ -67,5 +95,20 @@ clocking: system_clock_generation
         port map(
             input_clk_port  => clk_ext_port,     -- External clock
             system_clk_port =>  clk);   -- System clock   
+
+display : vga_test_pattern_12bit port map (
+	row => pixel_y,
+	column => pixel_x, 
+	color => color
+);
+
+sync : vga_sync port map (
+	clk => clk,
+	pixel_x => pixel_x,
+	pixel_y => pixel_y,
+	video_on => video_on,
+	hsync => Hsync_port,
+	vsync => Vsync_port
+);
     
 end behavioral_architecture;
--- vhdl/vga_sync.vhd
+++ vhdl/vga_sync.vhd
@@ -16,20 +16,99 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
 entity vga_sync is
-	port( clk	        		: in std_logic;
+	port( clk	        		: in std_logic; -- Assumes 25MHz clock
 		  pixel_x					: out std_logic_vector(9 downto 0);
           pixel_y					: out std_logic_vector(9 downto 0);
           video_on					: out std_logic;
           hsync					    : out std_logic;
           vsync					    : out std_logic);
-end vga_test_pattern;
+end vga_sync;
 
-architecture Behavioral of vga_test_pattern is
+architecture Behavioral of vga_sync is
 	
 -- signals
-signal 
+signal h_cnt, v_cnt : unsigned(9 downto 0) := (others => '0');
+signal h_sync_sig : std_logic := '0';
+signal H_video_on, V_video_on : std_logic := '0';
+
+
+constant left_border : integer := 48;
+constant h_display : integer := 640;
+constant right_border : integer := 16;
+constant h_retrace : integer := 96;
+constant H_MAX : integer := left_border + h_display + right_border + h_retrace - 1; --number of PCLKs in an H_sync period
+
+constant top_border : integer := 29;
+constant v_display : integer := 480;
+constant bottom_border : integer := 10;
+constant v_retrace : integer := 2;
+constant V_MAX : integer := top_border + v_display + bottom_border + v_retrace - 1; --number of H_syncs in an V_sync period
 
 begin
+
+h_counter : process(clk)
+begin
+    if rising_edge(clk) then
+        h_sync_sig <= '0';
+
+        if h_cnt < h_display + right_border then               
+            h_sync_sig <= '1';                    -- display and front porch
+        elsif h_cnt < h_display + right_border + h_retrace then
+            h_sync_sig <= '0';                    -- active sync pulse during retrace
+        else
+            h_sync_sig <= '1';                    -- back porch
+        end if;
+
+        h_cnt <= h_cnt + 1;
+        if h_cnt = H_MAX then   
+            h_cnt <= (others => '0');
+        end if;
+    end if; 
+end process h_counter;
+hsync <= h_sync_sig;
+
+v_counter : process(clk)
+begin
+    if rising_edge(clk) then
+        if h_cnt = H_MAX then
+            v_cnt <= v_cnt + 1;
+            if v_cnt = V_MAX then
+                v_cnt <= (others => '0');
+            end if;
+        end if;
+
+        if v_cnt < v_display + bottom_border then
+            vsync <= '1';
+        elsif v_cnt < display + bottom_border + v_retrace then
+            vsync <= '0';
+        else 
+            vsync <= '1';
+        end if;
+    end if; 
+end process v_counter;
+
+output_logic : process(clk) 
+begin
+    if rising_edge(clk) then
+        H_video_on <= '0';
+        V_video_on <= '0';
+        if h_cnt < h_display then
+            pixel_x <= std_logic_vector(h_cnt);
+            H_video_on <= '1';
+        else 
+            pixel_x <= (others => '0');
+        end if;
+
+        if v_cnt < v_display then
+            pixel_y <= std_logic_vector(v_cnt);
+            V_video_on <= '1';
+        else 
+            pixel_y <= (others => '0');
+        end if;
+    end if;
+end process output_logic;
+
+video_on <= H_video_on AND V_video_on; --Only enable video out when H_video_out and V_video_out are high. It's important to set the output to zero when you aren't actively displaying video. That's how the monitor determines the black level.
 
 end Behavioral;
 
